10f21526d02431ab6f524ae84548b51d82a8222a
vferdinandova <vesela.ferdinandova@trivago.com>
2017-03-12 17:58:13 +0100

    final changes

--- app/DoctrineMigrations/Version20170312134414.php
+++ app/DoctrineMigrations/Version20170312134414.php
@@ -16,7 +16,6 @@ class Version20170312134414 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('INSERT INTO user (username, username_canonical, email, email_canonical, enabled, salt, password, last_login, confirmation_token, password_requested_at, roles) VALUES (\'admin\', \'admin\', \'vesela.ferdinandova@gmail.com\', \'vesela.ferdinandova@gmail.com\', 1, null, \'$2y$13$Zg4oQwaLkqVYGrlZfDgTPuWGiHc5SFBHRDYK7uvjVs7OeyVhlD/d.\', \'2017-03-12 14:09:57\', null, null, \'a:1:{i:0;s:16:"ROLE_SUPER_ADMIN";}\');');
         $this->addSql('INSERT INTO category (name) VALUES (\'Mobile\');');
         $this->addSql('INSERT INTO manufacture (name, category_id) VALUES (\'SONY\', 1), (\'IPHONE\', 1);');
         $this->addSql('INSERT INTO attribute (name, label, type, category_id) VALUES (\'detail_description\', \'Detail Description\', 5, 1), (\'inches\', \'Inches\', 3, 1);');
--- run.sh
+++ run.sh
@@ -11,4 +11,7 @@ php bin/console doctrine:database:create
 php bin/console doctrine:migrations:migrate
 php bin/console server:start --forse
 
+php bin/console fos:user:create admin admin@admin admin --super-admin=1
+php bin/console fos:user:create user user@user user
+
 echo "Bye!
